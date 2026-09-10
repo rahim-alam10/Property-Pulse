6d35d4c9a8a22c623f588badcc1e8241e6f430ce
rahim-alam10 <rahimalam010@gmail.com>
2026-09-11 00:51:56 +0500

    Add loading and not found page components; update dependencies

---
+++ app/loading.jsx
@@ -0,0 +1,21 @@
+'use client'
+import {ClipLoader} from "react-spinners"
+
+const loading = ({ loading }) => {
+    const override = {
+        display: "block",
+        margin: "100px auto",
+    };
+    return (
+        <ClipLoader
+            color='#3b82f6'
+            loading={loading}
+            cssOverride={override}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+        />
+    )
+}
+
+export default loading

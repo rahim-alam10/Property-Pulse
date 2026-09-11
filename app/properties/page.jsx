a0e675adb469378adda4baa708526f79c9031e1b
rahim-alam10 <rahimalam010@gmail.com>
2026-09-11 23:13:36 +0500

    Refactor property fetching logic into a separate utility module and update components to use the new fetchProperties function

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,19 +1,5 @@
-import properties from '@/properties.json';
-
-async function fetchProperties(){
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
-
-    if(!res.ok){
-      throw new Error('Failed to fetch data');
-    }
-
-    return res.json();
-
-  } catch (error) {
-    console.log("Fetch Properties Error: ",error)
-  }
-}
+import PropertyCard from "@/components/PropertyCard";
+import { fetchProperties } from "../../utils/requests";
 
 const PropertiesPage = async() => {
   const properties = await fetchProperties();
